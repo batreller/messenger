cefd53e6329b44dc80bd54e4138303433410bba0
batreller <batreller@gmail.com>
2023-01-09 22:31:55 +0000

    enable cors

--- messenger/settings.py
+++ messenger/settings.py
@@ -47,7 +47,6 @@ else:
 
 INSTALLED_APPS = [
     'channels',
-    'corsheaders',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -61,9 +60,6 @@ INSTALLED_APPS = [
 ASGI_APPLICATION = 'messenger.asgi.application'
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
